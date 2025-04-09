1dad52dd131ee9abbbe86623972efd6d9ef15abd
unknown <princesahu17125@gmail.com>
2025-04-09 11:30:21 +0530

    9th of april

--- cmt.java
+++ cmt.java
@@ -1,9 +1,29 @@
 public class first{
+
 public static void main(String[] args){
-    System.out.println("commiting");
-}
+    int m=5;
+    byte a=11;
+    byte b=30;
+    
+    // System.out.println("Addition "+(a+b)) ;
+    // System.out.println("sub "+(a-b)) ;
+    // System.out.println("multiplicaiton "+(a*b)) ;
+    // System.out.println("division "+ ((float)a/b)) ;
+    
+    // System.out.println("division "+ (b%a)) ;
+
+    char  ch='C';
+    char  cha='A';
+
+    if (ch>='A' && ch<='Z') System.out.println("Uppercase");
+    if (ch>='a'&& ch<='z') System.out.println("lowercase");
+    int i=1;
+    while(true){
+        i++;
+        System.out.println(i);
+    }
+
+  
+    }
 
 }
-/*arary,linkedlist,stack,hash table,
-
-*/
